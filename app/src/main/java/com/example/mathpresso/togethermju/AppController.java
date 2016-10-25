ebf8514389ec677a05b1e295153de947aee345e3
ChoiJinjoo <jinjoooooooo@gmail.com>
2016-10-25 13:25:06 +0900

    add retrofit, recyclerview in builld.gradle dependencies

---
+++ app/src/main/java/com/example/mathpresso/togethermju/AppController.java
@@ -0,0 +1,27 @@
+package com.example.mathpresso.togethermju;
+
+import android.app.Application;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+/**
+ * Created by choijinjoo on 2016. 10. 25..
+ */
+public class AppController extends Application {
+    private static AppController mInstance;
+    private Retrofit mRetrofit;
+    private String baseUrl = "우리 서버주소";
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+    public Retrofit getRetrofit(){
+        mRetrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build();
+
+    return mRetrofit;
+    }
+}
