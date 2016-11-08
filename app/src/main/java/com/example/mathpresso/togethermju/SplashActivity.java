b0f0902ddffc0ec6c3588a08172b18b6e8c02154
ChoiJinjoo <jinjoooooooo@gmail.com>
2016-11-08 18:03:36 +0900

    create splash activity

---
+++ app/src/main/java/com/example/mathpresso/togethermju/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.mathpresso.togethermju;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                Intent intent = new Intent(SplashActivity.this, TempActivity.class);
+                startActivity(intent);
+
+                finish();
+
+            }
+        };
+
+        handler.sendEmptyMessageDelayed(0, 2000);
+    }
+}
---
+++ app/src/main/java/com/example/mathpresso/togethermju/TempActivity.java
@@ -0,0 +1,25 @@
+package com.example.mathpresso.togethermju;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class TempActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp);
+        Button btnGotoMainActivity = (Button)findViewById(R.id.btnMain);
+        btnGotoMainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TempActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
