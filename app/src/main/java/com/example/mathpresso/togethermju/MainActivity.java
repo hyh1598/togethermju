5e43f5800bd089bad900ba3999d0bb08e9d39abe
ChoiJinjoo <jinjoooooooo@gmail.com>
2016-11-09 02:47:27 +0900

    main activity layout

--- app/src/main/java/com/example/mathpresso/togethermju/MainActivity.java
+++ app/src/main/java/com/example/mathpresso/togethermju/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.mathpresso.togethermju;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String[] TAB_TITLES = {
+            "공지사항",
+            "공모전",
+            "봉사활동",
+            "스포츠"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        viewPager.setAdapter(new simpleAdapter(getSupportFragmentManager()));
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
+    private class simpleAdapter extends FragmentPagerAdapter {
+        public simpleAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return TAB_TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new NoticeFragment();
+                case 1:
+                    return new ContestFragment();
+                case 2:
+                    return new VoluntaryFragment();
+                case 3:
+                    return new SportsFragment();
+            }
+
+            throw new IllegalStateException("There's no fragment for position " + position);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TAB_TITLES[position];
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/mathpresso/togethermju/NoticeFragment.java
@@ -0,0 +1,91 @@
+package com.example.mathpresso.togethermju;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * Created by choijinjoo on 2016. 11. 9..
+ */
+public class NoticeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_notice, container, false);
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new SimpleAdapter(recyclerView));
+        return rootView;
+    }
+    private static class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.ViewHolder> {
+        private static final int UNSELECTED = -1;
+
+        private RecyclerView recyclerView;
+        private int selectedItem = UNSELECTED;
+
+        public SimpleAdapter(RecyclerView recyclerView) {
+            this.recyclerView = recyclerView;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.recycler_item_notice, parent, false);
+            return new ViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.bind(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 100;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+            private TextView expandButton;
+            private int position;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                expandButton = (TextView) itemView.findViewById(R.id.expand_button);
+
+                expandButton.setOnClickListener(this);
+            }
+
+            public void bind(int position) {
+                this.position = position;
+
+                expandButton.setText(position + ". Tap to expand");
+
+                expandButton.setSelected(false);
+            }
+
+            @Override
+            public void onClick(View view) {
+                ViewHolder holder = (ViewHolder) recyclerView.findViewHolderForAdapterPosition(selectedItem);
+                if (holder != null) {
+                    holder.expandButton.setSelected(false);
+                }
+
+                if (position == selectedItem) {
+                    selectedItem = UNSELECTED;
+                } else {
+                    expandButton.setSelected(true);
+                    selectedItem = position;
+                }
+            }
+        }
+    }
+
+}
