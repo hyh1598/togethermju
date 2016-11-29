9f2c51a446c73c1f92cd0fc30f2abae7f66d610f
DESKTOP-FJG3HNS\mk <tjdahrrud@naver.com>
2016-11-29 23:09:19 +0900

    second

--- app/src/main/java/com/example/mathpresso/togethermju/NoticeDetailsActivity.java
+++ app/src/main/java/com/example/mathpresso/togethermju/NoticeDetailsActivity.java
@@ -5,21 +5,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mathpresso.togethermju.RegisterActivity.GroupResiterActivity;
 import com.example.mathpresso.togethermju.adapter.ListViewAdapter;
+import com.example.mathpresso.togethermju.model.Notice;
 import com.melnykov.fab.FloatingActionButton;
 
 public class NoticeDetailsActivity extends AppCompatActivity{
+    ListView listview;
+    ListViewAdapter adapter;
+    private TextView textViewTitle;
+    private TextView textViewContent;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_details);
+        getIntent();
 
-        ListView listview;
-        ListViewAdapter adapter;
 
+        Intent intent = getIntent();
+        Notice notice = (Notice) intent.getSerializableExtra("notice");
+        String title = notice.getTitle();
+        String content = notice.getContent();
+
+        textViewTitle = (TextView) findViewById(R.id.notice_detail_title);
+        textViewContent = (TextView) findViewById(R.id.notice_detail_content);
+
+        textViewTitle.setText(title);
+        textViewContent.setText(content);
         // Adapter 생성
         adapter = new ListViewAdapter();
 
--- app/src/main/java/com/example/mathpresso/togethermju/NoticeFragment.java
+++ app/src/main/java/com/example/mathpresso/togethermju/NoticeFragment.java
@@ -1,5 +1,6 @@
 package com.example.mathpresso.togethermju;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Toast;
 
 import com.example.mathpresso.togethermju.adapter.NoticeAdapter;
@@ -35,10 +37,15 @@ public class NoticeFragment extends Fragment {
         RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
+
         mAdapter = new NoticeAdapter(recyclerView, null, getActivity(), new NoticeAdapter.OnNoticeSelectedListener() {
             @Override
-            public void onSelect(Notice contest) {
-                Toast.makeText(getActivity(), contest.getTitle(), Toast.LENGTH_SHORT).show();
+            public void onSelect(Notice notice) {
+                Toast.makeText(getActivity(), notice.getTitle(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), NoticeDetailsActivity.class);
+
+                intent.putExtra("notice",notice);
+                startActivity(intent);
             }
         });
         recyclerView.setAdapter(mAdapter);
@@ -47,12 +54,15 @@ public class NoticeFragment extends Fragment {
         return rootView;
     }
 
+
+
     private void loadNotices() {
         AppController.getInstance().getRestManager().getNoticeService().getNotices()
                 .enqueue(new Callback<List<Notice>>() {
                     @Override
                     public void onResponse(Call<List<Notice>> call, Response<List<Notice>> response) {
                         if (response.isSuccess()) {
+
                             mAdapter.clear();
                             mAdapter.add(response.body());
                         }
