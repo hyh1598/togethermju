36063075c97194f793afe375e6c90f379135dae8
손지호 <jihoson94@gmail.com>
2016-12-01 06:35:35 +0900

    notice detail layout 수정

--- app/src/main/java/com/example/mathpresso/togethermju/NoticeDetailsActivity.java
+++ app/src/main/java/com/example/mathpresso/togethermju/NoticeDetailsActivity.java
@@ -1,16 +1,14 @@
 package com.example.mathpresso.togethermju;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import com.example.mathpresso.togethermju.RegisterActivity.GroupResiterActivity;
 import com.example.mathpresso.togethermju.adapter.ListViewAdapter;
@@ -26,10 +24,10 @@ import retrofit2.Response;
 public class NoticeDetailsActivity extends AppCompatActivity {
     ListView listview;
     ListViewAdapter adapter;
-    private TextView textViewTitle;
+    private TextView txtvTitle;
     private TextView textViewContent;
-    private Button watch;
-    ToggleButton tb;
+    private LinearLayout btnWatch;
+    private TextView txtvWatch;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,28 +40,27 @@ public class NoticeDetailsActivity extends AppCompatActivity {
         String title = notice.getTitle();
         String content = notice.getContent();
 
-        textViewTitle = (TextView) findViewById(R.id.notice_detail_title);
+        txtvTitle = (TextView) findViewById(R.id.txtvTitle);
         textViewContent = (TextView) findViewById(R.id.notice_detail_content);
-        watch = (Button) findViewById(R.id.watch);
-        tb = (ToggleButton) this.findViewById(R.id.watch);
+        btnWatch = (LinearLayout) this.findViewById(R.id.btnWatch);
+        txtvWatch = (TextView) findViewById(R.id.txtvWatch);
 
-        tb.setOnClickListener(new View.OnClickListener() {
+
+        btnWatch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (tb.isChecked()) {
-                    tb.setTextColor(Color.BLUE);
+                if (btnWatch.isSelected()) {
                     postWatchNotice(notice);
                     //서버로 watch +1
                 } else {
-                    tb.setTextColor(Color.RED);
                     postWatchNotice(notice);
                     //서버로 watch값 -1
                 }
             }
         });
 
-        textViewTitle.setText(title);
-        textViewContent.setText(content);
+        txtvTitle.setText(title);
+        textViewContent.setText(content.trim());
         String email = AppController.getInstance().getStringValue("email", "hardho@naver.com");
 
         AppController.getInstance().getRestManager().getNoticeService().watchNotice("hardho@naver.com", notice.getNoticeSeq())
@@ -72,20 +69,18 @@ public class NoticeDetailsActivity extends AppCompatActivity {
                     public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response) {
                         if (response.isSuccess()) {
                             if (response.body().getResult().equals("watch")) {
-                                tb.setSelected(true);
-                                tb.setText("WATCH");
+                                initWatchBtn(true);
 
 
                             } else {
-                                tb.setSelected(false);
-                                tb.setText("UNWATCH");
+                                initWatchBtn(false);
                             }
                         }
                     }
 
                     @Override
                     public void onFailure(Call<DefaultResponse> call, Throwable t) {
-                        Log.d("watch",t.toString());
+                        Log.d("watch", t.toString());
                     }
                 });
         // Adapter 생성
@@ -116,24 +111,33 @@ public class NoticeDetailsActivity extends AppCompatActivity {
         //adapter.addItem(ContextCompat.getDrawable(this, ));
 
     }
-    private void postWatchNotice(Notice notice){
+
+    private void postWatchNotice(Notice notice) {
         AppController.getInstance().getRestManager().getNoticeService().watchNotice("hardho@naver.com", notice.getNoticeSeq())
-            .enqueue(new Callback<DefaultResponse>() {
-                @Override
-                public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response) {
-                    if(response.isSuccess()){
-                        if(response.body().getResult().equals("watch")){
-                            tb.setSelected(true);
-                        }else{
-                            tb.setSelected(false);
+                .enqueue(new Callback<DefaultResponse>() {
+                    @Override
+                    public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response) {
+                        if (response.isSuccess()) {
+                            if (response.body().getResult().equals("watch")) {
+                                initWatchBtn(true);
+                            } else {
+                                initWatchBtn(false);
+                            }
                         }
                     }
-                }
 
-                @Override
-                public void onFailure(Call<DefaultResponse> call, Throwable t) {
+                    @Override
+                    public void onFailure(Call<DefaultResponse> call, Throwable t) {
 
-                }
-            });
+                    }
+                });
+    }
+
+    private void initWatchBtn(boolean watch) {
+        if (watch) {
+            txtvWatch.setText("WATCH");
+        } else {
+            txtvWatch.setText("UNWATCH");
+        }
     }
 }
